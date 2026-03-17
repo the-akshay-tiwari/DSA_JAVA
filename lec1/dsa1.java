6ee1f8d94a1cfea5ebf470fecabec194b02091ac
akshay201104 <akshaytqpwo@gmail.com>
2026-03-17 11:48:57 +0530

    basics of java

---
+++ lec1/dsa1.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class dsa1 {
+  public static void main(String arg[]){
+    //System.out.print("hello \njava world");
+    // System.out.print("\nhiiiiii");
+    // System.out.print("\nhiiiii");
+    // String a="akshay";
+    // System.out.print(a);
+  
+    
+
+    //for input 
+    Scanner sc = new Scanner((System.in));
+    //finding sum with user input
+    
+    /*System.out.print("Enter first number : ");
+    int a = sc.nextInt();
+    System.out.print("Enter second number : ");
+    int b = sc.nextInt();
+    
+    System.out.print(a+b);*/
+    
+    /* 
+    //area of a circle 
+    System.out.print("enter the radius of the circle : ");
+    float r = sc.nextFloat();
+    float area = 3.14f*r*r;
+    System.out.print("The area of the circle is : "+area);
+    */
+
+    //type casting 
+    /*float a = 3.56f;
+    int b = (int)a;
+    System.out.println(a);
+    System.out.println(b);*/
+
+    /*long f=12345678;
+    float g=f;*/
+
+    sc.close();
+
+
+    //type promotion in expression
+
+    char a='a';
+    char b='b';
+    System.out.println(b+a);//answer 1
+    String s1="a";
+    String s2="b";
+    System.out.println(s1+s2);
+
+  }
+ } 
