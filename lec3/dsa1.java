5e7213264e9049e263a1af6ce20f06892a3ea658
akshay201104 <akshaytqpwo@gmail.com>
2026-03-17 11:50:32 +0530

    loops in java

---
+++ lec3/dsa1.java
@@ -0,0 +1,17 @@
+package lec3;
+
+public class dsa1 {
+  public static void main(String[] args) {
+    //print 1-10
+    /*for(int i=1;i<=10;i++){
+      System.out.println(i);
+      }
+    */
+   
+      //print 1 to n
+      int n=6;
+      for(int i=0;i<=n;i++){
+        System.out.println(i);
+      }
+  }
+}
