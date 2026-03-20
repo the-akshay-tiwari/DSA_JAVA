e2561e3c32f50f8e16f36c6757f3d75b4a505558
akshay201104 <akshaytqpwo@gmail.com>
2026-03-20 14:08:58 +0530

    Recurssion

--- lec12Recurssion/dsa9.java
+++ lec12Recurssion/dsa9.java
@@ -1,5 +1,36 @@
 package lec12Recurssion;
 
 public class dsa9 {
-  
+  //Binary strings problem
+  //print all binary strings of size N without consecutive ones.
+
+  public static void binStr(int n,int lastPlace, String str){
+    if(n==0){
+      System.out.println(str);
+      return;
+    }
+
+    binStr(n-1, 0, str+"0");
+    if(lastPlace==0){
+      binStr(n-1, 1, str+"1");
+    }
+
+    /*
+    //0 on last seat
+    if(lastPlace==0){
+      binStr(n-1, 0, str.append("0"));
+      binStr(n-1, 1, str.append("1"));
+    }
+    //1 on last seat
+    else{
+      binStr(n-1, 0, str.append("0"));
+    }
+    */
+
+  }
+
+  public static void main(String[] args) {
+    binStr(4, 0, "");
+  }
+
 }
