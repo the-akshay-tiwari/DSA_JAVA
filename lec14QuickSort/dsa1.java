7bc0bd51b5ba4091af2607a3d9aa39d758833652
akshay201104 <akshaytqpwo@gmail.com>
2026-03-21 23:15:13 +0530

    Quick sort

---
+++ lec14QuickSort/dsa1.java
@@ -0,0 +1,14 @@
+package lec14QuickSort;
+
+public class dsa1 {
+  //divide and conquer or specifically we can say:
+  //pivot and partition
+  //quick sort
+
+
+
+  public static void main(String[] args) {
+    
+  }
+
+}
